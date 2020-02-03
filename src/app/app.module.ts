2975deebb29ad82bd5eb7c29437c5ae236dfb837
Michael <mihendricks1@gmail.com>
2020-02-03 17:21:28 +0200

    Adding and getting request from api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
 
 import { SharedModule } from "./shared/shared.module";
 
@@ -19,7 +20,8 @@ import { SharedModule } from "./shared/shared.module";
     BrowserAnimationsModule,
     SharedModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/services/request.service.ts
+++ src/app/services/request.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@angular/core";
 import { RequestModel } from "../component/request/request.model";
 import { Observable, of, BehaviorSubject } from "rxjs";
+import { HttpClient } from "@angular/common/http";
 
 @Injectable({
   providedIn: "root"
@@ -8,19 +9,28 @@ import { Observable, of, BehaviorSubject } from "rxjs";
 export class RequestService {
   requestList: RequestModel[];
   requestObserver: BehaviorSubject<RequestModel[]>
+  url = "http://localhost:5000/request";
 
-  constructor() {
+  constructor(private http: HttpClient) {
     this.requestList = [];
     this.requestObserver = new BehaviorSubject<RequestModel[]>(this.requestList);
   }
 
   getRequests(): Observable<RequestModel[]> {
+    this.http.get(this.url).subscribe((items: RequestModel[]) => {
+      this.requestList = items;
+      this.requestObserver.next(this.requestList);
+    })
     return this.requestObserver;
   }
 
   addRequest(requestModel: RequestModel): Observable<RequestModel> {
     this.requestList.push(requestModel);
     this.requestObserver.next(this.requestList);
+    this.http.post(this.url, requestModel).subscribe((item: RequestModel) => {
+      this.requestList.push(item);
+      this.requestObserver.next(this.requestList);
+    });
     return of(requestModel);
   }
 }
